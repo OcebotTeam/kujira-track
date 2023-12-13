57f04cff144be2e9ebf80d60ad782e728aae07fa
Juan Carlos Molina <jcmolina@ocebot.com>
2023-12-13 12:16:20 +0100

    Total volume done.

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -1,28 +1,14 @@
 <?php
 
 namespace App\Controller;
-
 use App\Entity\BowTvl;
-use App\Entity\StakedManta;
-use App\Entity\StakedTokens;
-use App\Entity\Transactions;
-use App\Entity\Unmigrated;
-use App\Entity\UskMinted;
-use App\Entity\Wallets;
-use App\Repository\BowTvlRepository;
 use App\Service\ApplicationGlobalsService;
-use DateInterval;
-use DateTime;
-use DateTimeInterface;
 use Doctrine\ORM\EntityManagerInterface;
-use PhpParser\Builder\Class_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Validator\Constraints\Date;
-use Symfony\Contracts\Cache\ItemInterface;
 
 
 class ApiController extends AbstractController
@@ -57,9 +43,13 @@ class ApiController extends AbstractController
 
     public function volume (ApplicationGlobalsService $application_globals, string $precision = '1D'){
 
+        $request = Request::createFromGlobals();
+        $from = $request->query->get('from');
+        $to = $request->query->get('to');
         $volume = [];
+        $volume[] = $this->_all_fin_pair_volume($application_globals, $precision, $from, $to);
 
-        $volume[] = $this->_all_fin_pair_volume($application_globals, $precision);
+
 
 
         //$cache = new FilesystemAdapter();
@@ -69,7 +59,7 @@ class ApiController extends AbstractController
 
 
         return new Response(
-            json_encode(["FIN CONTRACTS" => $volume]),
+            json_encode(["totalVolume" => $volume]),
             Response::HTTP_OK,
             [
                 'Content-Type' => 'application/json',
@@ -86,7 +76,7 @@ class ApiController extends AbstractController
 
         $fin_urls = $application_globals->get_fin_contracts();
         $pair_value = $fin_urls[$pair];
-        $volume = $this->_fin_pair_volume($pair_value['contract'],'1D');
+        $volume = $this->_fin_pair_info($pair_value['contract'],'1D');
         return new Response(
            json_decode($volume),
             Response::HTTP_OK,
@@ -149,48 +139,58 @@ class ApiController extends AbstractController
 
     /* Calculations methods.*/
 
-    private function _fin_pair_volume(string $contract, string $precision)  {
+    private function _fin_pair_info(array $contract, string $precision, string $from, string $to)  {
 
-        //Format date = '2022-11-29T13:00:00.000Z'
-        $end_date = new DateTime('now');
-        $end_date_parameter = $end_date->format(DateTimeInterface::ISO8601);
-        $start_date = $end_date->sub(new DateInterval('P1Y'));
-        $start_date_parameter = $start_date->format(DateTimeInterface::ISO8601);
         $client = HttpClient::create();
         $response = $client->request(
             'GET',
-            "https://kaiyo-1.gigalixirapp.com/api/trades/candles?contract=" . $contract . "&precision=". $precision . "&from=" . str_replace('+','.',$start_date_parameter) ."&to=". str_replace('+','.',$end_date_parameter)
+            "https://kaiyo-1.gigalixirapp.com/api/trades/candles?contract=" . $contract['contract'] . "&precision=". $precision . "&from=" . $from ."&to=". $to
         );
-            return json_decode($response->getContent());
+        return json_decode($response->getContent());
 
     }
 
-    private function _all_fin_pair_volume(ApplicationGlobalsService $applicationGlobalsService, $precision = '1D') {
+    private function _all_fin_pair_volume(ApplicationGlobalsService $applicationGlobalsService, $precision = '1D', $from , $to) {
         $fin_contracts = $applicationGlobalsService->get_fin_contracts();
 
         $volume  = [];
         $volume[] = [];
 
+        for ($i = 0 ; $i < count($fin_contracts); $i++){
+            $volume[$i]['date'] = 0;
+            $volume[$i]['volume'] = 0;
+        }
+
+       $nominatives =  $this->_get_nominatives();
+        $nominativeCandles = [];
+       for($i = 0; $i < count($nominatives) ; $i++){
+           $nominativeCandles[$nominatives[$i]] = $this->_fin_pair_info($this->_get_contract($nominatives[$i]), $precision, $from, $to);
+       }
+
         foreach ($fin_contracts as $key => $value){
-            foreach($this->_fin_pair_volume($value['contract'],$precision) as $pairVolume){
-                dump ($pairVolume);
-               for ($i = 0 ; $i < count($pairVolume); $i++){
-                    $volume[$i]['date'] = 0;
-                    $volume[$i]['volume'] = 0;
-                }
+            $pairCandles = $this->_fin_pair_info($value, $precision, $from, $to);
 
-                for ($i = 0 ; $i < count($pairVolume); $i++){
-                    $date = $this->_format_date($pairVolume[$i]->bin);
-                    $volume[$i]['date'] = $date;
-                    $volume[$i]['volume'] += $pairVolume[$i]->volume ;
-                }
+            $candles = $pairCandles->candles;
+            for ($i = 0 ; $i <count($candles); $i++){
+                $date = $candles[$i]->bin;
+                $volume[$i]['date'] = $date;
 
+                if(isset($value['nominative']) && $nomin_key = array_search($value['nominative'], $nominatives)){
+                    $nominative = $nominativeCandles[$nominatives[$nomin_key]];
+                    $price = $nominative->candles[$i]->close;
+                    $volume[$i]['volume'] += $candles[$i]->volume * $price;
+
+                    if (isset($value['priceMultiplier'])) {
+                        $volume[$i]['volume'] /= $value['priceMultiplier'];
+                    }
+                } else {
+                    $volume[$i]['volume'] += $candles[$i]->volume  ;
+                }
             }
 
         }
-        dump($volume);
 
-        //return $all_candles;
+        return $volume;
     }
 
     private function _get_fin_pairs(){
@@ -213,6 +213,24 @@ class ApiController extends AbstractController
     {
         $date = explode('T', $date);
         return $date[0];
+    }
+
+    private function _get_contract($ticker){
+        $this->application_globals = new ApplicationGlobalsService();
+        $contracts = $this->application_globals->get_fin_contracts();
+        return $contracts[$ticker];
+    }
+
+    private function _get_nominatives(){
+        $this->application_globals = new ApplicationGlobalsService();
+        $contracts = $this->application_globals->get_fin_contracts();
+        $nominative_tickers = [];
+        foreach ($contracts as $contract){
+            if(isset($contract['nominative']))
+                if(!in_array($contract['nominative'], $nominative_tickers))
+                    $nominative_tickers[] = $contract['nominative'];
+        }
+        return $nominative_tickers;
     }
 
 
