8e4c8885126b60f5cc6cc8ed346f72f3612485bc
Pedro Delgado <pedelgado@users.noreply.github.com>
2024-04-03 18:02:35 +0000

    Allow any origing while in dev environment

--- app/Controller/FinContractCharts/GetFinContractChart.php
+++ app/Controller/FinContractCharts/GetFinContractChart.php
@@ -8,12 +8,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 class GetFinContractChart extends AbstractController
 {
     public function __construct(
         private readonly FinContractChartRequester $chartRequest,
-        private readonly FinContractFinder $contractFinder
+        private readonly FinContractFinder $contractFinder,
+        private readonly KernelInterface $kernel
     ) {
     }
 
@@ -22,8 +24,22 @@ class GetFinContractChart extends AbstractController
     {
         $timeframe = $request->query->get('timeframe', 'daily');
         $page = $request->query->get('page', 0);
+
         $contract = $this->contractFinder->__invoke($tickerId);
         $chart = $this->chartRequest->__invoke($contract['address'], $timeframe, $page);
-        return new JsonResponse($chart);
+
+        $response = new JsonResponse($chart);
+        
+        // Get the current environment
+        $environment = $this->kernel->getEnvironment();
+
+        // Set CORS headers only in 'dev' environment
+        if ($environment === 'dev') {
+            $response->headers->set('Access-Control-Allow-Origin', '*');
+            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept');
+        }
+
+        return $response;
     }
 }
