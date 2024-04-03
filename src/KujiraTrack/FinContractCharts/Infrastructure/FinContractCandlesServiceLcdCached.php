e2aab687e0aca24b221a523fef799d1f8920d9c7
Pedro Delgado <pedelgado@users.noreply.github.com>
2024-04-03 18:09:44 +0000

    FinContractCandlesServiceLcdCached refactor to just cache what FinContractCandlesServiceLcd returns and reuse its logic

--- src/KujiraTrack/FinContractCharts/Infrastructure/FinContractCandlesServiceLcdCached.php
+++ src/KujiraTrack/FinContractCharts/Infrastructure/FinContractCandlesServiceLcdCached.php
@@ -2,24 +2,20 @@
 
 namespace Ocebot\KujiraTrack\FinContractCharts\Infrastructure;
 
-use Ocebot\KujiraTrack\FinContractCharts\Domain\FinContractCandle;
-use Ocebot\KujiraTrack\FinContractCharts\Domain\FinContractCandleDateTime;
 use Ocebot\KujiraTrack\FinContractCharts\Domain\FinContractCandles;
-use Ocebot\KujiraTrack\FinContractCharts\Domain\FinContractCandlesService;
 use Ocebot\KujiraTrack\FinContractCharts\Domain\TimeFrame;
 use Ocebot\KujiraTrack\FinContracts\Domain\FinContractAddress;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
-class FinContractCandlesServiceLcdCached implements FinContractCandlesService
+class FinContractCandlesServiceLcdCached extends FinContractCandlesServiceLcd
 {
-    private const CANDLES_ENDPOINT = "https://kaiyo-1.gigalixirapp.com/api/trades/candles";
-
     public function __construct(
-        private readonly HttpClientInterface $httpClient,
+        HttpClientInterface $httpClient,
         private readonly CacheInterface $cache
     ){
+        parent::__construct($httpClient);
     }
 
 
@@ -30,36 +26,10 @@ class FinContractCandlesServiceLcdCached implements FinContractCandlesService
         return $this->cache->get($cacheKey, function (ItemInterface $item) use ($address, $timeframe, $page) {
             $item->expiresAfter(1800); // Cache for 30 minutes
 
-            $toAmountBack = -$page * self::BATCH_SIZE;
-            $fromAmountBack = $toAmountBack - self::BATCH_SIZE + 1;
+            // Call the parent method and cache its result
+            $result = parent::requestCandles($address, $timeframe, $page);
 
-            $fromDate = new FinContractCandleDateTime($fromAmountBack . ' ' . $timeframe->dateTimeKey());
-            $toDate = new FinContractCandleDateTime($toAmountBack . ' ' . $timeframe->dateTimeKey());
-
-            $response = $this->httpClient->request('GET', self::CANDLES_ENDPOINT, [
-                "query"  => [
-                    "contract" => $address->value(),
-                    "precision" => $timeframe->apiKey(),
-                    "from" => $fromDate->value(),
-                    "to" => $toDate->value(),
-                ]
-            ]);
-
-            $candlesArray = [];
-            $responseContent = json_decode($response->getContent());
-
-            foreach ($responseContent->candles as $candle) {
-                $candlesArray[] = new FinContractCandle(
-                    (float) $candle->low,
-                    (float) $candle->high,
-                    (float) $candle->close,
-                    (float) $candle->open,
-                    $candle->bin,
-                    (int) $candle->volume
-                );
-            }
-
-            return new FinContractCandles($candlesArray);
+            return $result;
         });
     }
 }
