424b5475ca0c0e400750ee51a632cd10c67c35f8
Juan Carlos Molina <jcmolina@ocebot.com>
2024-07-08 16:47:50 +0200

    limit still needeed

--- app/Controller/UskMintedController.php
+++ app/Controller/UskMintedController.php
@@ -41,7 +41,7 @@ class UskMintedController extends AbstractController
     {
 
         $usk_minted_repo = $entityManager->getRepository(UskMinted::class);
-        $usk_minted_all = $usk_minted_repo->findBy([], ["tracked" => "ASC"]);
+        $usk_minted_all = $usk_minted_repo->findBy([], ["tracked" => "ASC"],null,30000);
         $result = [];
 
         foreach($usk_minted_all as $item) {
